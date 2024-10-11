d44e21a732d655fafb62828590bdd49efd1ff0af
jared <jj.math.j@gmail.com>
2024-10-11 01:23:26 +0000

    progress

--- demos/rectangle.js
+++ demos/rectangle.js
@@ -26,6 +26,50 @@ export function loadDemo(demoContainerElement) {
         rectangleVertex_Ccc,
         rectangleVertex_Ddd,
     ];
+    const bigTriangleVertex_Eee = [
+        0,
+        Math.sqrt(
+            rectangleHeight * rectangleHeight + rectangleWidth * rectangleWidth,
+        ) / rectangleHeight * rectangleHypotenuse,
+    ];
+    const bigTriangleVertex_Fff = [
+        Math.sqrt(
+            rectangleHeight * rectangleHeight + rectangleWidth * rectangleWidth,
+        ) / rectangleWidth * rectangleHypotenuse,
+        0,
+    ];
+    const bigTriangleVertices = [
+        // rectangleVertex_Aaa,
+        bigTriangleVertex_Eee,
+        bigTriangleVertex_Fff,
+    ];
+    const midTriangleVertex_Ggg = [
+        bigTriangleVertex_Fff[0] - (bigTriangleVertex_Eee[1] - rectangleVertex_Bbb[1]),
+        0,
+    ];
+    const midTriangleVertices = [
+        // rectangleVertex_Aaa,
+        // rectangleVertex_Bbb, 
+        midTriangleVertex_Ggg,
+    ];
+    const smallTriangleVertex_Hhh = [
+        0,
+        bigTriangleVertex_Eee[1] - rectangleVertex_Bbb[1]
+    ];
+    const smallTriangleVertices = [
+        // rectangleVertex_Aaa,
+        smallTriangleVertex_Hhh,
+        // rectangleVertex_Ddd
+    ];
+    const otherSmallTriangleVertex_Iii = [
+        rectangleVertex_Ccc[0],
+        rectangleVertex_Ccc[1] - (bigTriangleVertex_Eee[1] - rectangleVertex_Bbb[1])
+    ]
+    const otherSmallTriangleVertices = [
+        // rectangleVertex_Bbb,
+        // rectangleVertex_Ccc,
+        otherSmallTriangleVertex_Iii
+    ];
     const svgNS = "http://www.w3.org/2000/svg";
     const svgElement = document.createElementNS(svgNS, "svg");
     svgElement.setAttribute(
@@ -37,16 +81,28 @@ export function loadDemo(demoContainerElement) {
     const diagramTransformElement = document.createElementNS(svgNS, "g");
     diagramTransformElement.setAttribute(
         "transform",
-        `translate(0, ${diagramHeight}) scale(1,-1)`
+        `translate(0, ${diagramHeight}) scale(1,-1)`,
     );
     svgElement.appendChild(diagramTransformElement);
-    rectangleVertices.forEach((someRectangleVertex) => {
-        const vertexCircleElement = document.createElementNS(svgNS, "circle");
-        vertexCircleElement.setAttribute("cx", someRectangleVertex[0]);
-        vertexCircleElement.setAttribute("cy", someRectangleVertex[1]);
-        vertexCircleElement.setAttribute("r", 0.01);
-        vertexCircleElement.setAttribute("fill", "red");
-        diagramTransformElement.appendChild(vertexCircleElement);
-    });
+    [
+        ...rectangleVertices,
+        ...bigTriangleVertices,
+        ...midTriangleVertices,
+        ...smallTriangleVertices,
+        ...otherSmallTriangleVertices
+    ]
+        .forEach(
+            (someRectangleVertex) => {
+                const vertexCircleElement = document.createElementNS(
+                    svgNS,
+                    "circle",
+                );
+                vertexCircleElement.setAttribute("cx", someRectangleVertex[0]);
+                vertexCircleElement.setAttribute("cy", someRectangleVertex[1]);
+                vertexCircleElement.setAttribute("r", 0.01);
+                vertexCircleElement.setAttribute("fill", "red");
+                diagramTransformElement.appendChild(vertexCircleElement);
+            },
+        );
     demoContainerElement.appendChild(svgElement);
 }
